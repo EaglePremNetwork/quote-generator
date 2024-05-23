487908bfd544215fd39d0c2aa9bd956ec759eee0
Prem <prem.fcr@gmail.com>
2024-05-23 10:56:17 +0530

    add loader

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote() {
+  loading();
   // Pick a radom quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // Check if author field is blank and replace it with "Unknown"
@@ -22,17 +36,21 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
+  // Set Quote & (show quote & hide loader)
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
     newQuote();
   } catch (error) {}
+  complete();
 }
 
 // Tweet Quote
